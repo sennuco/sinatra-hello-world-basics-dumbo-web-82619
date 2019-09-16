4b84917960b73442f90b758576748593e4a21f9c
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-16 19:56:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ class App < Sinatra::Base
 
   get '/' do
     "Hello, World!"
-  end# Define your GET '/' route below and respond with "Hello, World!"
+  end
+  
+  # Define your GET '/' route below and respond with "Hello, World!"
 
 
 end
